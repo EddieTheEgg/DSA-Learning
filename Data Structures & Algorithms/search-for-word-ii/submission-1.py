5f9a20c511b11a3b990a5c61f8de22ed67ec2b6b
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2026-05-10 00:34:44 -0400

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,59 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {} #max length of 26 character path for the chidl per letter
+        self.isWord = False #Naturally most letters are not the end, but the last letter of words we set as True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        #We first should build out tree, like as we iterate we
+        # add our found letter to a children level but idk if
+        # we should build our tree first or just iterate through
+
+        #Step 1. First turn words list into a trie path
+        trie_path = TrieNode()
+
+        def insertWord(word : str):
+            curr = trie_path
+            for char in word:
+                if char not in curr.children:
+                    curr.children[char] = TrieNode()
+                curr = curr.children[char]
+            curr.isWord = True
+
+        for word in words:
+            insertWord(word)
+        
+        #Step 2: Backtracking through the grid and using out trie_path
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        valid_words = set()
+        visit = set()
+
+        def dfs(row, col, node, word):
+            #Base case, a bad cell we need to backtrack up
+            if row >= ROWS or col >= COLS or row < 0 or col < 0 or (row, col) in visit or board[row][col] not in node.children:
+                return
+
+            #Update current node/visited to the new cell we are now checking
+            visit.add((row,col))
+            node = node.children[board[row][col]]
+
+            word += board[row][col]
+            if node.isWord:
+                valid_words.add(word)
+            
+            dfs(row + 1, col, node, word)
+            dfs(row - 1, col, node, word)
+            dfs(row, col + 1, node, word)
+            dfs(row, col - 1, node, word)
+
+            #Remember to remove cell we've visited as we backtrack up
+            #so we can try other directions
+            visit.remove((row, col))
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, trie_path, "")
+
+        return list(valid_words)
